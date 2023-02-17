0ec02d6b2696e2b11c244af169724fc25fb6d064
Sam Maisha <sam.maisha99@gmail.com>
2023-02-17 09:01:35 +0000

    update getUserWithEmail, getUserWithId and addUser functions to interact directly  with the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,16 +18,15 @@ const pool = new Pool ({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  userEmail = email.toLowerCase();
+  const queryString = `
+  SELECT id, name, email FROM users
+  WHERE email = $1`;
+  const values = [userEmail];
+
+  return pool
+  .query(queryString, values)
+  .then((result) => result.rows[0] || null);
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +36,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+  SELECT id, name, email, FROM users
+  WHERE id = $1`;
+  const values = [id];
+  
+  pool
+  .query(queryString, values)
+  .then((result) => result.rows[0] || null);
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +54,17 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  queryString = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *
+  `
+  values = [user.name, user.email, user.password]
+  pool
+  .query(queryString, values)
+  .then((result) => {
+    return result.rows[0];
+  })
 }
 exports.addUser = addUser;
 
@@ -85,7 +98,6 @@ const getAllProperties = function(options, limit = 10) {
   return pool
   .query(queryString, values)
   .then((result) => {
-    console.log(result.rows);
     return result.rows;
   })
   .catch((err) => {
